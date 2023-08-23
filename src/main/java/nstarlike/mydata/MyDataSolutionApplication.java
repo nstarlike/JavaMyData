ea7376ee449630d4f6c628312f2a703ebb65a52f
nstarlike <nstar0512@gmail.com>
2023-08-23 23:42:52 +0900

    Initialize a Spring Boot project

---
+++ src/main/java/nstarlike/mydata/MyDataSolutionApplication.java
@@ -0,0 +1,13 @@
+package nstarlike.mydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDataSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDataSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nstarlike/mydata/MyDataSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package nstarlike.mydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDataSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
