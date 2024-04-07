05f6ce93d1aab9b8f0a6a2235b1e2d5f49d15995
nstarlike <nstar0512@gmail.com>
2024-04-07 16:07:20 +0900

    Create a Spring Boot application

---
+++ mock-ca/src/main/java/nstarlike/app/MockCaApplication.java
@@ -0,0 +1,11 @@
+package nstarlike.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MockCaApplication.class, args);
+	}
+}
