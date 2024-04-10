d60a462a5594d1103e88d3474ce91233ed464b5e
nstarlike <nstar0512@gmail.com>
2024-04-10 19:02:14 +0900

    Add SpringBootApplication

---
+++ mock-other-org/src/main/java/nstarlike/mock/org/MockOtherOrgApplication.java
@@ -0,0 +1,13 @@
+package nstarlike.mock.org;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MockOtherOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockOtherOrgApplication.class, args);
+	}
+
+}
